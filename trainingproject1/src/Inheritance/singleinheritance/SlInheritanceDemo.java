dad97d97fe915817a54271dff5ad73f872f09e2e
Logeshwaran-U <logeshwaranuk@gmail.com>
2025-08-19 14:16:34 +0530

    Single Inheritance

---
+++ trainingproject1/src/Inheritance/singleinheritance/SlInheritanceDemo.java
@@ -0,0 +1,17 @@
+package Inheritance.singleinheritance;
+
+
+
+	public class SlInheritanceDemo {
+
+		public static void main(String[] args) {
+			
+			trainer t = new trainer(001,"linkesh","Puducherry",01,"SOFTWARE DEVELOPER","Chennai");
+			System.out.println(t);
+			
+			person a = new person(113,"linkesh","Puducherry");
+			System.out.println(a);
+			
+		}
+
+}
