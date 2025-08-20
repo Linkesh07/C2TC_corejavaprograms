4759c4cae4ae4d75465226f06ab56daf800508c7
STUDENT <STUDENT@DESKTOP-0PQTLC6>
2025-08-20 14:16:47 +0530

    Abstract example completed

---
+++ trainingproject1/src/abstract_example/SoftSkillTrainer.java
@@ -0,0 +1,13 @@
+package abstract_example;
+
+public class SoftSkillTrainer extends Trainer {
+	@Override
+	public void session() {
+		System.out.println("Resume Building is going on");
+	}
+	@Override
+	public void checks() {
+		System.out.println("Cover letter assignments");
+	}
+
+}
